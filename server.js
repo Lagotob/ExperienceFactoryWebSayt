60968b8ac46ac375c24849bede5a818a1f918b78
Lagotob <lagotob763@gmail.com>
2026-05-05 14:10:35 +0500

    Update server to use environment variable for port

--- server.js
+++ server.js
@@ -38,6 +38,10 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server 3000-portda ishlamoqda..."));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server ${PORT}-portda ishlamoqda...`);
+});
 
 app.use(express.static('public'));
