adae33d660d67a351ba3f16132476fc0943d9015
Lagotob <lagotob763@gmail.com>
2026-05-05 15:10:08 +0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -20,7 +20,7 @@ async function registerUser() {
     };
 
     try {
-        const response = await fetch('/index.html', { 
+        const response = await fetch('/', { 
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
